9effbc16bcb1ed85c9f795b35dc37a3f27b9ef2d
Panaitov Yurii <81642643+panaitoff@users.noreply.github.com>
2024-01-23 17:47:30 +0300

    Add files via upload

---
+++ dae/js/script.js
@@ -0,0 +1,9 @@
+new Swiper('.pick__body', {
+    spaceBetween: 30,
+    loop: true,
+    slidesPerView: 3,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
